e3dc7f1a355ff7f2b059d07bae36447b88c1a0d5
Jelani Woods <jelani@firstdraft.com>
2019-05-10 14:42:41 -0500

    Fix typos

--- lib/generators/draft/spike/spike_generator.rb
+++ lib/generators/draft/spike/spike_generator.rb
@@ -4,12 +4,17 @@ require "generators/draft/spike/spike_controller_generator"
 require "rails/generators/rails/spike/spike_generator"
 
 module Draft
-  source_root File.expand_path('../templates', __FILE__)
-
   class SpikeGenerator < Rails::Generators::Base
+    source_root File.expand_path('../templates', __FILE__)
+
+    remove_hook_for :template_engine
 
     def generate_controller
       invoke Draft::SpikeControllerGenerator
+    end
+
+    def generate_views
+      invoke Draft::SpikeErbGenerator
     end
   end
 end
