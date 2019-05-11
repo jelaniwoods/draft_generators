8e4b18370823112ff0ee7613a3bbcc25cc0713c2
Jelani Woods <jelani@firstdraft.com>
2019-05-11 09:51:49 -0500

    wip

--- lib/generators/draft/spike/spike_controller_generator.rb
+++ lib/generators/draft/spike/spike_controller_generator.rb
@@ -4,9 +4,10 @@ require "generators/draft/spike/spike_erb_generator"
 require "rails/generators/rails/spike_controller/spike_controller_generator"
 
 module Draft
-  class SpikeControllerGenerator < ::Rails::Generators::ScaffoldControllerGenerator
-    source_root Rails::Generators::ScaffoldControllerGenerator.source_root
+  class SpikeControllerGenerator < Rails::Generators::NamedBase
+
     source_root File.expand_path('../templates', __FILE__)
+    argument :attributes, type: :array, default: [], banner: "field:type field:type"
 
     def generate_views
       invoke Draft::SpikeErbGenerator
