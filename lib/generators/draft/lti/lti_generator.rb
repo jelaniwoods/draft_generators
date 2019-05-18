a2b94956a6557310000952e2ae4ae7de2f617cee
Jelani Woods <jelani@firstdraft.com>
2019-05-18 14:51:33 -0500

    wip

--- lib/generators/draft/lti/lti_generator.rb
+++ lib/generators/draft/lti/lti_generator.rb
@@ -4,6 +4,7 @@ module Draft
 
     argument :attributes, type: :array, default: [], banner: "field:type field:type"
 
+
     def generate_controller
       template "controllers/controller.rb", "app/controllers/#{plural_table_name.underscore}_controller.rb"
     end
@@ -11,7 +12,7 @@ module Draft
     def generate_model
       invoke "draft:model"
     end
-    
+
     def create_root_folder
       empty_directory File.join("app/views", plural_table_name)
     end
@@ -27,62 +28,50 @@ module Draft
       scaffold_routes
     end
 
-    private 
-  
-      def launch_and_config_routes
-        log :route, "Launch and Config routes"
+  private
 
-        route <<-RUBY.gsub(/^      /, "")
+    def scaffold_routes
+      log :route, "Scaffolding routes"
+      route = ""
+      if singular_table_name == "launch"
+        route = route <<-RUBY.gsub(/^      /, "")
 
-          root "application#landing"
-          get "/landing", to: "application#landing", as: "landing"
-          get "/config" => "launches#xml_config"
+          resources plural_table_name.to_sym, only: :create
+        RUBY
+      end
+      if singular_table_name == "administrator"
+        route = route <<-RUBY.gsub(/^      /, "")
+
+          devise_for plural_table_name.to_sym
         RUBY 
+
+      else
+        route = route <<-RUBY.gsub(/^      /, "")
+
+          resources plural_table_name.to_sym, only: %i[]
+        RUBY         
       end
-      
-      def scaffold_routes
-        log :route, "Scaffolding routes"
+      route
+    end
 
-        route = ""
-        if singular_table_name == "launch"
-          route = route <<-RUBY.gsub(/^      /, "")
 
-            resources plural_table_name.to_sym, only: :create
-          RUBY
-        end
-        if singular_table_name == "administrator"
-          route = route <<-RUBY.gsub(/^      /, "")
+    def route(routing_code)
+      sentinel = /\.routes\.draw do(?:\s*\|map\|)?\s*$/
 
-            devise_for plural_table_name.to_sym
-          RUBY 
-
-        else
-          route = route <<-RUBY.gsub(/^      /, "")
-
-            resources plural_table_name.to_sym, only: %i[]
-          RUBY         
-        end
-        route
+      inside "config" do
+        insert_into_file "routes.rb", routing_code, after: sentinel
       end
-      
-      def route(routing_code)
-        sentinel = /\.routes\.draw do(?:\s*\|map\|)?\s*$/
+    end
 
-        inside "config" do
-          insert_into_file "routes.rb", routing_code, after: sentinel
-        end
-      end
+    def available_views
+      %w(index new edit show _card)
+    end
 
-      def available_views
-        %w(index new edit show _card)
-      end
-  
-      def view_filename_with_extensions(name)
-        filename = [name, :html, :erb].compact.join(".")
-        folders = ["views"]
-        filename = File.join(folders, filename) if folders.any?
-        filename
-      end
+    def view_filename_with_extensions(name)
+      filename = [name, :html, :erb].compact.join(".")
+      folders = ["views"]
+      filename = File.join(folders, filename) if folders.any?
+      filename
     end
   end
 end
